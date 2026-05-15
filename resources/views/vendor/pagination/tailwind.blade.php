1816fe9174c2bf94e64612df26e842f4bad43eff
Daniel Ngwasi <damnngwasi@gmail.com>
2026-05-15 18:37:08 +0300

    fix: force add custom pagination view

---
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -0,0 +1,33 @@
+@if ($paginator->hasPages())
+<nav class="pagination">
+    {{-- Previous --}}
+    @if ($paginator->onFirstPage())
+        <span class="disabled">‹</span>
+    @else
+        <a href="{{ $paginator->previousPageUrl() }}" rel="prev">‹</a>
+    @endif
+
+    {{-- Page Numbers --}}
+    @foreach ($elements as $element)
+        @if (is_string($element))
+            <span class="dots">{{ $element }}</span>
+        @endif
+        @if (is_array($element))
+            @foreach ($element as $page => $url)
+                @if ($page == $paginator->currentPage())
+                    <span class="active"><span>{{ $page }}</span></span>
+                @else
+                    <a href="{{ $url }}">{{ $page }}</a>
+                @endif
+            @endforeach
+        @endif
+    @endforeach
+
+    {{-- Next --}}
+    @if ($paginator->hasMorePages())
+        <a href="{{ $paginator->nextPageUrl() }}" rel="next">›</a>
+    @else
+        <span class="disabled">›</span>
+    @endif
+</nav>
+@endif
